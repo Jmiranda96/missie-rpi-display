99806912bfca5417f49bd669411df134616084a9
Jorge M <jorgemiranda.co@gmail.com>
2023-11-28 02:28:55 -0500

    update init

--- script/MissieInterface.py
+++ script/MissieInterface.py
@@ -6,6 +6,9 @@ from PIL import ImageTk, Image
 import pygame
 import vlc
 
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 class REMInterface():
 
